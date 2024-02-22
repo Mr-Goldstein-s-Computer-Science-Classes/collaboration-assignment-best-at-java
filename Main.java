be90669c33069d487beb12faffc29c9b71559152
Niyati <113116250+Niyati2007@users.noreply.github.com>
2024-02-22 13:27:17 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
 
+        Gradebook grades = new Gradebook();
+
+        //Create Students
+
+        grades.addAssignment("Stacks And Queues Station Activity");
+        grades.addAssignment("Stacks and queues programs");
+        grades.addAssignment("Stacks and queues quiz");
+        grades.addAssignment("Github Exploration");
+
+        grades.gradeAssignment("Stacks and queues station", "Tyler", 94);
+
+
     }
 }
