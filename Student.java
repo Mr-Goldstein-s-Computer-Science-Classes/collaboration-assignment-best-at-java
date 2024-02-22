d25649706bb15cc2715f4ad2c9b8f2d655918b5c
Niyati <113116250+Niyati2007@users.noreply.github.com>
2024-02-22 13:05:42 -0500

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,15 +1,21 @@
+import java.util.List;
+import java.util.ArrayList;
 public class Student {
 
     private String name;
+    private List<String> assignments;
+    private List<Double> assignmentGrades;
 
     public Student(String name)
     {
         this.name = name;
+        assignments = new ArrayList<String>();
+        assignmentGrades = new ArrayList<Double>();
     }
 
     public void addAssignment(String assignName)
     {
-
+        assignments.add(assignName);
     }
 
     public void gradeAssignment(String assignment, double grade)
@@ -29,7 +35,7 @@ public class Student {
 
     public String toString()
     {
-
+        return name + ": " + getOverallGrade();
     }
 
 }
